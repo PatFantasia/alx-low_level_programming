419405c7bf81330a2472958308f2d17fef6215c8
PatFantasia <desouza.paatrice@gmail.com>
2022-12-08 08:27:49 -0800

    Use prinf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,13 +2,11 @@
 
 /**
  * main - prints the phrase "Programming is like building a multilingual puzzle
-
  * Return: 0 if exited properly, non-zero otherwise
-
  */
 
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art, ");
-  return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
