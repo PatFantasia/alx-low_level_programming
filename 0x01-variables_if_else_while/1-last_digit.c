8c49229b104b481df9b3a885894108737fe1b04b
PatFantasia <desouza.paatrice@gmail.com>
2022-12-09 09:34:21 -0800

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,6 +22,6 @@ int main(void)
 	else if (lastDigit == 0)
 		printf("and is 0\n");
 	else if (lastDigit < 6 && lastDigit != 0)
-		printf("is less than 6 and not 0\n");
+		printf("and is less than 6 and not 0\n");
 	return (0);
 }
