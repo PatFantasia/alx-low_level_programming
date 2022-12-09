239889e3537f182723d8db9938f3d636d0093f4b
PatFantasia <desouza.paatrice@gmail.com>
2022-12-09 14:21:11 -0800

    Variable and if conditions

---
+++ 0x01-variables_if_else_while/tmp/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ * main :  print the state of n
+ * n : random value
+ * return : always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("is positve \n");
+	else if (n < 0)
+		printf("is negative \n");
+	else
+	{
+		printf("is zero \n");
+	}
+	return (0);
+}
