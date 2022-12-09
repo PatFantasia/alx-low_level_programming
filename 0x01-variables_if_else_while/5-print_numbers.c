6cdefde495d9bfb1b3555e6e11675f03b3a48041
PatFantasia <desouza.paatrice@gmail.com>
2022-12-09 13:31:54 -0800

    Variable and if conditions

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Print sigle digit numbers
+ * of base 10 from 0
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch <= 10; ch++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
