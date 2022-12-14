24568af4b04eb2bb36a67deb9e36f4f512845fb5
PatFantasia <desouza.paatrice@gmail.com>
2022-12-14 03:56:15 -0800

    functions nested loops tasks

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * times_table - print 9 times table
+ */
+
+void times_table(void)
+{
+	int i = 0;
+
+	int j, k;
+
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 10)
+		{
+			k = i * j;
+			if (j < 9)
+			{
+				_putchar(k + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else
+				_putchar(k + '0');
+			_putchar('\n');
+			j++;
+		}
+		i++;
+	}
+}
