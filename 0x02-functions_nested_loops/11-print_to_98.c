728b3a3d97573324fdeb302216d2540c4ed0fcad
PatFantasia <desouza.paatrice@gmail.com>
2022-12-14 09:27:05 -0800

    functions nested loops tasks

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - print from n to 98
+ * @n: given number from which start the increment
+ */
+
+void print_to_98(int n)
+{
+	int i, j;
+
+	if (n >= 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			i == 98 ? printf("%d\n", i) : printf("%d, ", i);
+		}
+	}
+	else if (n <= 98)
+	{
+		for (j = n; j <= 98; j++)
+		{
+			j == 98 ? printf("%d\n", i) : printf("%d, ", j);
+		}
+	}
+}
