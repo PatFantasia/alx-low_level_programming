df9fca04765a55a37a15d9b4bc5bf114c9fde07f
PatFantasia <desouza.paatrice@gmail.com>
2022-12-13 21:27:37 -0800

    functions nested loops tasks

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sing - prints the sign of an input
+ * @n: the input to check sign
+ * Return : 0 if input is equal to zero,
+ * 1 if it greater than zero and -1 if it less than zero
+ */
+
+int print_sing(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
