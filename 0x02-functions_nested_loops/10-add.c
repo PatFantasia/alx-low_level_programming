4dc89c52eacbdd202d577eb3610104dc83533535
PatFantasia <desouza.paatrice@gmail.com>
2022-12-14 08:30:39 -0800

    functions nested loops tasks

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add to integers and return their result
+ * @a: integer to be added to b
+ * @b: integer to be added to a
+ * Return: the sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
