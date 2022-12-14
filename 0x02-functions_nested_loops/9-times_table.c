5c1bd7278f50c38345a31f25894a830c0b587c00
PatFantasia <desouza.paatrice@gmail.com>
2022-12-14 07:12:54 -0800

    functions nested loops tasks

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,36 +6,31 @@
 
 void times_table(void)
 {
-	int i = 0;
+	int i, j, k;
 
-	int j, k;
-
-	while (i < 10)
+	for (i = 0; i < 10; i++)
 	{
-		j = 0;
-		while (j < 10)
+		for (j = 0; j < 10; j++)
 		{
-			k = i * j;
+			k = j * i;
 			if (j == 0)
-				_putchar(k + '0');
-			if (j < 10 && j != 0)
 			{
-				if (k < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-				}
-				else if (k >= 10)
-				{
-					_putchar(' ');
-				}
-				if (k >= 10)
-					_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
-				j == 9 ? _putchar('\n') : _putchar(',');
+				_putchar(k + '0');
 			}
-			j++;
+			if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			} else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
 		}
-		i++;
+		_putchar('\n');
 	}
 }
