9121b1c80ca0e28631c337b29f382684efc05cbe
PatFantasia <desouza.paatrice@gmail.com>
2022-12-15 06:17:33 -0800

    more functions

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line, ended by a new line on the terminal
+ * @n: the number of times '/' should be printed
+ * just return a new line if n is zero or negative
+ */
+
+void print_diagonal(int n)
+{
+	int counter, space_counter;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (counter = 0; counter < n; counter++)
+		{
+			space_counter = 0;
+			while (space_counter < (counter + 1))
+			{
+				_putchar(' ');
+				space_counter++;
+			}
+			_putchar('\');
+			_putchar('\n');
+		}
+
+	}
+
+}
