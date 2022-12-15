16d927534465432173a188e82dc04a7e8a3403b2
PatFantasia <desouza.paatrice@gmail.com>
2022-12-15 12:08:59 -0800

    more functions

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,9 +11,9 @@
 
 int main(void)
 {
-	int i = 1;
+	int i;
 
-	while (i <= 100)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
@@ -31,7 +31,6 @@ int main(void)
 		{
 			printf(" %d", i);
 		}
-		i++;
 	}
 	printf("\n");
 
