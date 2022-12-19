674385b8f47ff90dee22c0216ea7e99c968fec00
PatFantasia <desouza.paatrice@gmail.com>
2022-12-19 15:29:36 -0800

    Pointers Arrays Strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps the values of two integers
+ * @a: first parameter
+ * @b: second parameter
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = *temp;
+}
