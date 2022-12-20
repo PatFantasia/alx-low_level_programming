8e15cf5ae8232eaee75f3bea71e1cb744e3ee47e
PatFantasia <desouza.paatrice@gmail.com>
2022-12-20 06:10:41 -0800

    Pointers Arrays Strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,9 +9,10 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0' && i % 2 == 0)
+	while (str[i] != '\0')
 	{
-		_putchar(str[i] + 0);
+		if (i % 2 == 0)
+			_putchar(str[i] + 0);
 		i++;
 	}
 	_putchar('\n');
