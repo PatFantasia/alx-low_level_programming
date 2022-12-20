3ccda4a1b7b2d7e09d3a9c24dda406f76c84a764
PatFantasia <desouza.paatrice@gmail.com>
2022-12-20 00:24:13 -0800

    Pointers Arrays Strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to stdout followed by a new line
+ * @str: string to print
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i] + '0');
+		i++;
+	}
+	_putchar('\n');
+}
