a68823e6e1fad0de8d16d4acc6b05d1d1d75b997
PatFantasia <desouza.paatrice@gmail.com>
2022-12-20 05:45:16 -0800

    Pointers Arrays Strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every odd character from a string
+ * @str: string to print
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0' && str[i] % 2 == 0)
+	{
+		_putchar(str[i] + 0);
+		i++;
+	}
+	_putchar('\n');
+}
