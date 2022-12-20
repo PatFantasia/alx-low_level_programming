aaceb1f4575a9d6f8469328cde8867b05b98c393
PatFantasia <desouza.paatrice@gmail.com>
2022-12-20 04:35:13 -0800

    Pointers Arrays Strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - prints a string in reversed
+ * followed by a new line
+ * @s: string to print
+ */
+
+void rev_string(char *s)
+{
+	int i = 0, j = 0;
+
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	char rev_output[i];
+
+	while (i >= 0)
+	{
+		if (s[i] != '\0')
+			rev_output[j] = s[i];
+		i--;
+		j++;
+	}
+}
