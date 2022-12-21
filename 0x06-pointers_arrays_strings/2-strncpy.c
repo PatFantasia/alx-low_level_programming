5aa1575f3769a1967daea9344444677f562854d3
PatFantasia <desouza.paatrice@gmail.com>
2022-12-21 09:51:53 -0800

    Pointers Arrays Strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,12 +11,9 @@
 char *_strncopy(char *dest, char *src, int n)
 {
 	int i = 0;
-
-	while (i < n && src[i] != '\0')
-	{
+ 
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-		i++;
-	}
 	while (i < n)
 	{
 		dest[i] = '\0';
