87f77bab314d4cf31b801c7514e150958be15dda
PatFantasia <desouza.paatrice@gmail.com>
2022-12-21 13:17:54 -0800

    Pointers Arrays Strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,8 +4,8 @@
  * _strncopy - copies two strings
  * @src: source of string to append
  * @dest: destination for appending src string
- * @n: add n byte to dest from src
- * Return: a pointer containing the copy
+ * @n: add n byte to @dest from @src
+ * Return: @dest a pointer containing the copy
  */
 
 char *_strncpy(char *dest, char *src, int n)
