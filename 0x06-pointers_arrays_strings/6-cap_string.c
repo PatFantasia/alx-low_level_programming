c6fd1052130ee09a2d6ac1749f999d9a05f8fad1
PatFantasia <desouza.paatrice@gmail.com>
2022-12-22 00:24:17 -0800

    Pointers Arrays Strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,7 +2,7 @@
 
 /**
  * cap_string - capitalizes all the words of a string
- * @s : the string
+ * @s: the string
  * Return: capitilized word
  */
 
@@ -10,15 +10,16 @@ char *cap_string(char *s)
 {
 	int i = 0, j = 0;
 
-	char sp[15];
+	/*char sp[15];*/
 
-	sp[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', ' ', '\n'};
+	char sp[] = ",;,.,!?\"(){} ";
+	/*{',', ';', '.', '!', '?', '"', '(', ')', '{', '}', ' ', '\n'};*/
 
 	while (s[i])
 	{
 		while (sp[j])
 		{
-			if (s[i] == sp[j])
+			if (s[i] == sp[j] || s[i] == '\n')
 			{
 				if (s[i + 1] && s[i + 1] >= 'a' && s[i] <= 'z')
 					s[i] = s[i] - 32;
