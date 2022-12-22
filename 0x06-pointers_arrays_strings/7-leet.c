c8beffc2ab47c4bc896c44b8dbda8b9c0c68053a
PatFantasia <desouza.paatrice@gmail.com>
2022-12-22 02:46:23 -0800

    Pointers Arrays Strings

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string to 1337
+ * @s: the string
+ * Return: encoded string  @s
+ */
+
+char *leet(char *s)
+{
+	int i = 0, j = 0;
+
+	char letter[] = "AaEeOoTtLl";
+	char code[] = "4433007711";
+
+	while (s[i])
+	{
+		while (letter[j])
+		{
+			if (s[i] == letter[j])
+				s[i] = code[j];
+			j++
+		}
+		i++;
+	}
+	return (s);
+}
