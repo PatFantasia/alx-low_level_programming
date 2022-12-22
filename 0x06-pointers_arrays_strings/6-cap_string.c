3a041d3c2a7d9ea212a02a09521ec763edaa5e27
PatFantasia <desouza.paatrice@gmail.com>
2022-12-22 00:09:26 -0800

    Pointers Arrays Strings

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all the words of a string
+ * @s : the string
+ * Return: capitilized word
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0, j = 0;
+
+	char sp[15];
+
+	sp[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', ' ', '\n'};
+
+	while (s[i])
+	{
+		while (sp[j])
+		{
+			if (s[i] == sp[j])
+			{
+				if (s[i + 1] && s[i + 1] >= 'a' && s[i] <= 'z')
+					s[i] = s[i] - 32;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (s);
+}
