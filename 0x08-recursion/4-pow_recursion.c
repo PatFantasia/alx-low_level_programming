f3f8f5afcb5cb7b10941cb11c51bd2684e8165a8
PatFantasia <desouza.paatrice@gmail.com>
2023-01-04 08:53:44 -0800

    Recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - return the value x raised to the power of y
+ * @x: the value to compute
+ * @y: the power
+ * Return: the computation result
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (0);
+	else if (y == 1)
+		return (x);
+	return (n * factorial(n - 1));
+
+}
