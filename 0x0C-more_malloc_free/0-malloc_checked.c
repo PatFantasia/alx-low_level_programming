ed439fbeffaf1f6224cb16f6bea318535c6c3e16
PatFantasia <desouza.paatrice@gmail.com>
2023-01-12 15:32:11 -0800

    More malloc free

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include "main.h"
 
 /**
  * malloc_checked - allocates memory using malloc
@@ -11,7 +12,7 @@
 
 void *malloc_checked(unsigned int b)
 {
-	void *new_mem;
+	char *new_mem;
 
 	new_mem = malloc(b);
 	if (new_mem == NULL)
